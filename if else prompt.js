e604043aee4742d3783a3d3b383494c3c0d85a12
iqbalal13 <123553063+iqbalal13@users.noreply.github.com>
2023-04-09 06:16:53 +0700

    Add files via upload

---
+++ if else prompt.js
@@ -0,0 +1,7 @@
+let number = prompt("Enter the time (in 24-hour format):");
+
+if (number > 10) {
+  console.log("The number is greater than 10.");
+} else {
+  console.log("The number is less than or equal to 10.");
+}
