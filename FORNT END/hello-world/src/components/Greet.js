9eaf7b7c788c4d32e9ca930e5ccf53d2dbba54ad
iqbalal13 <123553063+iqbalal13@users.noreply.github.com>
2023-08-11 15:19:38 +0700

    Update Greet.js

--- FORNT END/hello-world/src/components/Greet.js
+++ FORNT END/hello-world/src/components/Greet.js
@@ -2,7 +2,11 @@ import React from 'react'
 
 const Greet = (props) =>  {
  console.log(props)
- return<h1> {props.name} Jobdesc {props.job} </h1>
-}
+ return (
+    <div>
+ <h1> {props.name} Jobdesc {props.job} </h1>
+ {props.resigned}
+ </div>
+)}
 
 export default Greet
