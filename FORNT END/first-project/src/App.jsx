d47afa7a940291b04b453f44f9ca593e724f5447
iqbalal13 <123553063+iqbalal13@users.noreply.github.com>
2023-08-20 19:57:14 +0700

    Update App.jsx

--- FORNT END/first-project/src/App.jsx
+++ FORNT END/first-project/src/App.jsx
@@ -1,10 +1,17 @@
+import { useState } from "react"
 export default function App() {
+  const  [newItem, setNewItem] = useState("")
+
+  function handleSubmit(e) {
+    e.preventDefault()
+  }
   return (
     <>
-  <from className="new-item-form">
+  <from onSubmit={handleSubmit} className="new-item-form">
     <div className="form-row">
     <label htmlFor="item">New Item</label>
-    <input type="text" id="item" />
+    <input value={newItem} onChange={e => setNewItem(e.target.value)} 
+    type="text" id="item" />
     </div>
     <button className="btn">add</button>
   </from>
