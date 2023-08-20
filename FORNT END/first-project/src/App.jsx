7984987e4d4ed56cf8c01fca17dab5c1dfa91e78
iqbalal13 <123553063+iqbalal13@users.noreply.github.com>
2023-08-20 17:42:24 +0700

    Update App.jsx

--- FORNT END/first-project/src/App.jsx
+++ FORNT END/first-project/src/App.jsx
@@ -1,8 +1,11 @@
 export default function App() {
-  return <from className="new-item-form">
+  return (
+  <from className="new-item-form">
     <div className="form-row">
     <label htmlFor="item">New Item</label>
     <input type="text" id="item" />
     </div>
+    <button className="btn">add</button>
   </from>
+  )
 }
