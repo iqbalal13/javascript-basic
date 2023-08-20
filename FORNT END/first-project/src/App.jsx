e456d8dfc3c97e5102f8fff1f0c869765868a432
iqbalal13 <123553063+iqbalal13@users.noreply.github.com>
2023-08-20 18:00:54 +0700

    Update App.jsx

--- FORNT END/first-project/src/App.jsx
+++ FORNT END/first-project/src/App.jsx
@@ -1,5 +1,6 @@
 export default function App() {
   return (
+    <>
   <from className="new-item-form">
     <div className="form-row">
     <label htmlFor="item">New Item</label>
@@ -7,5 +8,26 @@ export default function App() {
     </div>
     <button className="btn">add</button>
   </from>
+  <h1 className="header">Todo List </h1>
+  <ul className="list">
+  <li>
+    <label>
+      <input type="checkbox" />
+      item 1
+
+    </label>
+    <button className="btn btn-danger">Delete</button>
+  </li>
+  <li>
+    <label>
+      <input type="checkbox" />
+      item 2
+
+    </label>
+    <button className="btn btn-danger">Delete</button>
+  </li>
+  </ul>
+
+  </>
   )
 }
