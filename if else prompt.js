6cd3072da450f1831136f1e7f8130b5c065e627d
iqbalal13 <muhammadiqbalalmaududii@gmail.com>
2023-10-21 18:24:54 +0700

    Delete if else prompt.js

--- if else prompt.js
+++
@@ -1,7 +0,0 @@
-let number = prompt("Enter the time (in 24-hour format):");
-
-if (number > 10) {
-  console.log("The number is greater than 10.");
-} else {
-  console.log("The number is less than or equal to 10.");
-}
