aa008a3d77d3ea3cbc197a619b115fe56a4bbb8d
iqbalal13 <muhammadiqbalalmaududii@gmail.com>
2023-10-22 16:54:31 +0700

    Add files via upload

---
+++ IF ELSE/else if prompt.js
@@ -0,0 +1,9 @@
+let time = prompt("Enter the time (in 24-hour format):");
+
+if (time < 12) {
+  console.log("Good morning!");
+} else if (time < 18) {
+  console.log("Good afternoon!");
+} else {
+  console.log("Good evening!");
+}
