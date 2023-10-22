467bd799f778c42be5d9ef1be0e5e5c67eb93173
iqbalal13 <muhammadiqbalalmaududii@gmail.com>
2023-10-22 16:58:35 +0700

    Delete else if prompt.js

--- else if prompt.js
+++
@@ -1,9 +0,0 @@
-let time = prompt("Enter the time (in 24-hour format):");
-
-if (time < 12) {
-  console.log("Good morning!");
-} else if (time < 18) {
-  console.log("Good afternoon!");
-} else {
-  console.log("Good evening!");
-}
