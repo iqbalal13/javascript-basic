c425aae5bae8352b6c7f57ee5f4610770259282d
iqbalal13 <muhammadiqbalalmaududii@gmail.com>
2023-10-24 18:56:57 +0700

    Add files via upload

---
+++ FOR LOOP/for in if else input.js
@@ -0,0 +1,14 @@
+let person = {
+    firstName: "",
+    lastName: "",
+    age: 0
+  };
+  
+  for (let prop in person) {
+    if (person.hasOwnProperty(prop)) {
+      let value = prompt("Enter " + prop);
+      person[prop] = value;
+    }
+  }
+  
+  console.log(person.firstName + " " + person.lastName + ", " + person.age);
